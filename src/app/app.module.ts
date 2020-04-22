83446a61693d586727e9ca8234a04729b9ab763f
Emiliano <emixda55@outlook.com>
2020-04-22 02:07:22 -0500

    cambios finales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
--- src/app/pelis/pelis.page.ts
+++ src/app/pelis/pelis.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import { MovieService } from '../services/movie.service';
 
 @Component({
@@ -11,6 +14,7 @@ export class PelisPage implements OnInit {
 
   information = null;
   showNotas: boolean;
+  pdfdata: any;
 
   constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
 
@@ -30,4 +34,34 @@ export class PelisPage implements OnInit {
     this.showNotas = !this.showNotas;
   }
 
+
+  generatepdf() {
+    let titulo = this.information.Title;
+    let sinopsis = this.information.Plot;
+    let poster = this.information.Poster;
+    let year = this.information.Year;
+    let rating = this.information.imdbRating;
+    let actors = this.information.Actors;
+    let genero = this.information.Genre;
+    let director = this.information.Director;
+
+
+    let docDefinition = {
+      content: [
+        { text: genero, fontSize: 22 },
+        { text: titulo, fontSize: 18 },
+        { text: year, fontSize: 15 },
+        { text: poster },
+        { text: sinopsis, fontSize: 12 },
+        { text: "Rating: " + rating, fontSize: 12 },
+        { text: "Director: " + director, fontSize: 12 },
+        { text: "Actores: " + actors, fontSize: 12 }
+      ]
+    };
+
+
+
+    this.pdfdata = pdfMake.createPdf(docDefinition);
+    this.pdfdata.download();
+  }
 }
